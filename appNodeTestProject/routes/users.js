6415e2e26c4a1dd2d97297900ca27e6865c3442f
wangwei21 <wangwei21@tuniu.com>
2018-04-16 19:50:46 +0800

    nodetest

---
+++ appNodeTestProject/routes/users.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    var conn = mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'mysqltest',
+        database: 'myfirsttest',
+        port: '3306'
+    });
+//进行连接
+    conn.connect();
+
+    var username = 'Miss w';
+    var course = 'Math';
+//以insert写sql语句
+    var sql = "select introduce from firsttable;";
+    conn.query(sql, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+            var aaa = {
+                "success": true,
+                "msg": "成功执行",
+                "errorCode": 200,
+                data:{
+                    "count": 10,
+                    "rows":result
+                }
+            }
+            res.send(aaa);
+        }
+    });
+});
+
+module.exports = router;
